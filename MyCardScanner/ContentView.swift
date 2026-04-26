48176e3fa4007085aa5c533a8fc506d2d4cfb115
YoshikiIzumi <continue_jump@icloud.com>
2026-04-26 18:18:09 +0900

    Initial Commit

---
+++ MyCardScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyCardScanner
+//
+//  Created by 泉芳樹 on 2026/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyCardScanner/MyCardScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardScannerApp.swift
+//  MyCardScanner
+//
+//  Created by 泉芳樹 on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyCardScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
